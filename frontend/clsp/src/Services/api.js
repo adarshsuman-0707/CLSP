6a6dcdd22e51ec33c064b3f4968c697b76413d8c
Adarsh suman <142964134+adarshsuman-0707@users.noreply.github.com>
2026-04-27 15:08:02 +0530

    Update api.js

--- frontend/clsp/src/Services/api.js
+++ frontend/clsp/src/Services/api.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "https://clspbackend-production.up.railway.app/api/";
 //  // Replace with your API URL
-const API_BASE_URL = "http://localhost:5000/api/"; // Replace with your API URL
+const API_BASE_URL = "https://clsp-backend.onrender.com/api/"; // Replace with your API URL
 
 export const endpoint = {
     //auth 
